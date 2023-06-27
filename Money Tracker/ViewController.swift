eb32e4b3601550c6423b9b96577951c99ce00f4c
Kelvin Gao <u6511753@au.edu>
2023-06-27 12:12:57 +0700

    Initial Commit

---
+++ Money Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Tracker
+//
+//  Created by Kelvin Gao  on 27/6/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
